d58c7f7bf1a044e5bf00ea449b3d4f8afa0b7694
CabreraLucas <lucas.cabrera@fatec.sp.gov.br>
2022-06-06 17:26:15 +0000

    Atividade2

---
+++ Atividade.java
@@ -0,0 +1,12 @@
+public class Atividade{
+    public static void main (String[] args){
+
+        int coisa[] = new int[5];
+
+        for (int index = 0; index < coisa.length; index++){
+          coisa[index] = index*10;
+        System.out.println(coisa[index]);
+
+        }
+    }
+}
